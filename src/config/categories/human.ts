b33acec0dc02bff6a29764864f5f68eb6a86827a
angulawk <agnieszka.weronika.wojcik@gmail.com>
2023-05-18 15:54:40 +0200

    Add images for Human subcategories

--- src/config/categories/human.ts
+++ src/config/categories/human.ts
@@ -1,14 +1,21 @@
 import { ICategoryProps } from "@app/components/molecules/Categories/@types/Categories";
 import { Path } from "@app/routing/paths";
 import { Title } from "@app/routing/titles";
+import Character from "@app/../public/icons/character.png";
+import HumanBody from "@app/../public/icons/body.png";
+import { IconDescription } from "@app/config/iconDescriptions";
 
 export const humanCategories: ICategoryProps[] = [
   {
     path: Path.CategoriesHumanBody,
-    title: Title.Body
+    title: Title.Body,
+    src: HumanBody,
+    description: IconDescription.HumanBody
   },
   {
     path: Path.CategoriesHumanCharacter,
-    title: Title.Character
+    title: Title.Character,
+    src: Character,
+    description: IconDescription.HumanCharacter
   }
 ];
--- src/config/iconDescriptions.ts
+++ src/config/iconDescriptions.ts
@@ -2,5 +2,7 @@ export enum IconDescription {
   Human = "Słownictwo związane z człowiekiem po hiszpańsku",
   Weather = "Słownictwo związane z pogodą po hiszpańsku",
   Profession = "Słownictwo związane z zawodami po hiszpańsku",
-  Animals = "Słownictwo związane ze zwierzętami po hiszpańsku"
+  Animals = "Słownictwo związane ze zwierzętami po hiszpańsku",
+  HumanBody = "Słownictwo związane z ciałem człowieka po hiszpańsku",
+  HumanCharacter = "Słownictwo związane z charakterem po hiszpańsku"
 }
