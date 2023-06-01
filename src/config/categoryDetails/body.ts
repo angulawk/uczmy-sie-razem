5a57dd2a0f9bd57ebd6cd89b781340ac0a79c209
angulawk <agnieszka.weronika.wojcik@gmail.com>
2023-06-01 14:17:16 +0200

    Add links to audio from storage

--- src/config/categoryDetails/body.ts
+++ src/config/categoryDetails/body.ts
@@ -3,26 +3,32 @@ import { ICategoryDetailsProps } from "@app/components/molecules/CategoryDetails
 export const bodyCategory: ICategoryDetailsProps["categoryDetails"] = [
   {
     word: "la cabeza",
-    wordAudio: "/audio/la_cabeza.mp3",
+    wordAudio:
+      "https://pub-27907ac1752340b79f24b92b358d3644.r2.dev/la_cabeza.mp3",
     wordTranslaion: "głowa",
     sentence: "Me duele la cabeza después de estudiar durante horas",
-    sentenceAudio: "/audio/la_cabeza_sentence.mp3",
+    sentenceAudio:
+      "https://pub-27907ac1752340b79f24b92b358d3644.r2.dev/la_cabeza_sentence.mp3",
     sentenceTranslation: "Boli mnie głowa po kilku godzinach nauki"
   },
   {
     word: "la cabeza",
-    wordAudio: "/audio/la_cabeza.mp3",
+    wordAudio:
+      "https://pub-27907ac1752340b79f24b92b358d3644.r2.dev/la_cabeza.mp3",
     wordTranslaion: "głowa",
     sentence: "Me duele la cabeza después de estudiar durante horas",
-    sentenceAudio: "/audio/la_cabeza_sentence.mp3",
+    sentenceAudio:
+      "https://pub-27907ac1752340b79f24b92b358d3644.r2.dev/la_cabeza_sentence.mp3",
     sentenceTranslation: "Boli mnie głowa po kilku godzinach nauki"
   },
   {
     word: "la cabeza",
-    wordAudio: "/audio/la_cabeza.mp3",
+    wordAudio:
+      "https://pub-27907ac1752340b79f24b92b358d3644.r2.dev/la_cabeza.mp3",
     wordTranslaion: "głowa",
     sentence: "Me duele la cabeza después de estudiar durante horas",
-    sentenceAudio: "/audio/la_cabeza_sentence.mp3",
+    sentenceAudio:
+      "https://pub-27907ac1752340b79f24b92b358d3644.r2.dev/la_cabeza_sentence.mp3",
     sentenceTranslation: "Boli mnie głowa po kilku godzinach nauki"
   }
 ];
